b4f239efa0592ba09ef90eb6ade1e8a42db199a9
dev__munjal <dev19kumar99@gmail.com>
2021-09-25 17:47:00 +0530

    changes

--- src/widgets/Menu/icons/index.ts
+++ src/widgets/Menu/icons/index.ts
@@ -27,4 +27,4 @@ export { default as RoadmapIcon } from "./Roadmap";
 export { default as BondsIcon } from "./Bonds";
 export { default as VaultsIcon } from "./Vaults";
 export { default as EggHouseIcon } from "./EggHouse";
-export { default as MediumIcon } from "./Medium";
+// export { default as MediumIcon } from "./Medium";
